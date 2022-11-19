b78ce0ba9a66e24215a159da1e3c38262769695e
Alex Kovacs <alexkovacs@rocketmail.com>
2022-11-19 03:58:29 +0200

    Set up the project

---
+++ src/main/java/ro/alexk/energyutilityplatformmeteringdevice/EnergyUtilityPlatformMeteringDeviceApplication.java
@@ -0,0 +1,13 @@
+package ro.alexk.energyutilityplatformmeteringdevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyUtilityPlatformMeteringDeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyUtilityPlatformMeteringDeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/alexk/energyutilityplatformmeteringdevice/EnergyUtilityPlatformMeteringDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.alexk.energyutilityplatformmeteringdevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyUtilityPlatformMeteringDeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
